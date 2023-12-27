508ad8daf506b7043efead24ecae97603f9af8fb
igor <i.voronezhtsev@gmail.com>
2023-12-27 19:36:43 +0300

    Переделал на использование Specification - это позволило избавится от внедрения EntityManager.

--- src/main/java/com/example/mapstruct/demo/EmployeeRepository.java
+++ src/main/java/com/example/mapstruct/demo/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package com.example.mapstruct.demo;
 
 import com.example.mapstruct.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Integer>, CriteriaRepository {
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>, JpaSpecificationExecutor<Employee> {
 }
--- src/main/java/com/example/mapstruct/demo/EmployeeService.java
+++ src/main/java/com/example/mapstruct/demo/EmployeeService.java
@@ -3,7 +3,9 @@ package com.example.mapstruct.demo;
 import com.example.mapstruct.demo.controller.request.EmployeeRequest;
 import com.example.mapstruct.demo.controller.response.EmployeesResponse;
 import com.example.mapstruct.demo.entity.Employee;
+import com.example.mapstruct.demo.entity.Employee_;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +18,9 @@ public class EmployeeService {
     private final EmployeeRequestResponseMapper employeeRequestResponseMapper;
 
     public List<EmployeesResponse> getEmployeesByFio(String fio) {
-        List<Employee> employees = employeeRepository.findByFio(fio);
+        List<Employee> employees = employeeRepository.findAll((Specification<Employee>) (root, query, criteriaBuilder)
+                -> criteriaBuilder.equal(root.get(Employee_.FIO), fio));
+
         return employeeRequestResponseMapper.toResponse(employees);
     }
 
